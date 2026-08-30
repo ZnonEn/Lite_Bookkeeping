37b323f0f7f18adb50a4e83b0477891a7a62e870
ZnonEn <110320851+ZnonEn@users.noreply.github.com>
2026-08-30 15:38:35 +0800

    版本号改为 0.2pre

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.nonen.Bookkeeping"
         minSdk = 26
         targetSdk = 37
-        versionCode = 1
-        versionName = "0.1beta"
+        versionCode = 2
+        versionName = "0.2pre"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
